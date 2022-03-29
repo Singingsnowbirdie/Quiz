a7858b4a5ed7bd83f14a8f4c642f7d076dba08d6
Elena Slavina <55747006+Singingsnowbirdie@users.noreply.github.com>
2022-03-29 08:48:18 +0300

    Project added

---
+++ Assets/Scripts/ChallengeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChallengeController : MonoBehaviour
+{
+
+    [SerializeField] Text infoText;
+    [SerializeField] InputField field;
+    int number;
+
+
+    void Start()
+    {
+        number = Random.Range(0, 101);
+    }
+
+    public void Press()
+    {
+        if (number > int.Parse(field.text))
+        {
+            infoText.text = "Загаданное число больше";
+        }
+        else if (number < int.Parse(field.text))
+        {
+            infoText.text = "Загаданное число меньше";
+        }
+        else
+        {
+            infoText.text = "Число угадано!";
+        }
+    }
+}
---
+++ Assets/Scripts/QuizController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class QuizController : MonoBehaviour
+{
+    int counter;
+    int step;
+
+    bool hintIsShown;
+
+    [SerializeField] List<GameObject> quizPanels;
+    List<GameObject> selectedQuizPanels;
+
+    [SerializeField] GameObject introPanel;
+    [SerializeField] GameObject hintPanel;
+    [SerializeField] GameObject winPanel;
+    GameObject currentActivePanel;
+
+    [SerializeField] TextMeshProUGUI counterText;
+    [SerializeField] TMP_InputField field;
+
+    void Start()
+    {
+        counter = 0;
+        step = 0;
+        currentActivePanel = introPanel;
+        currentActivePanel.SetActive(true);
+        selectedQuizPanels = new List<GameObject>();
+        selectedQuizPanels.AddRange(quizPanels);
+    }
+
+    public void WrongPress()
+    {
+        ShowNext();
+    }
+
+    public void CorrectPress()
+    {
+        counter++;
+        ShowNext();
+    }
+
+    public void CheckField(string correctString)
+    {
+        field = FindObjectOfType<TMP_InputField>();
+
+        if (correctString == field.text)
+        {
+            CorrectPress();
+        }
+        else
+        {
+            WrongPress();
+        }
+    }
+
+    public void ShowHint()
+    {
+        if (!hintIsShown)
+        {
+            hintPanel.SetActive(true);
+            hintIsShown = true;
+        }
+    }
+
+    void ShowNext()
+    {
+        if (currentActivePanel != null)
+        {
+            currentActivePanel.SetActive(false);
+
+            if (step < 10)
+            {
+                int rand = Random.Range(0, selectedQuizPanels.Count);
+                currentActivePanel = selectedQuizPanels[rand];
+                currentActivePanel.SetActive(true);
+                selectedQuizPanels.Remove(selectedQuizPanels[rand]);
+                step++;
+            }
+            else
+            {
+                winPanel.SetActive(true);
+                counterText.text = counter + "/10";
+            }
+        }
+    }
+
+    public void PressExit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
